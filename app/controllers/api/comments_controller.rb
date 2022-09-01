58edc26c8838f2327ff11909a86dc7ca17aa665e
Christian Green <hoolio.suare@gmail.com>
2022-08-31 21:01:18 -0600

    comments routes model

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,25 @@
+class Api::CommentsController < ApplicationController
+  #model name = comment
+  # follow the pattern to fill the controller out
+  # then change to the parent child relationship - @parent.children
+
+  #-optional- callback, before_action to clean up the show update and destroy
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,69 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # root - only rails, define the home page , but don't need since 
+  # we are using react 
 
-  # root - only rails, define the home page, but don't need since we are using react.
-  # Defines the root path route ("/")
-  # root "articles#index"
-  # routes - a way to have react communicate / do CRUD actions to rails, talk from front to back
-  # backend as api, all of our controllers are in a 
-  # this will point from react to somewhere in the controller.
-
-  namespace :api do
-    # this is where all of our routes will be
+  # routes - a way to have react communicate / do 
+  #   CRUD action to rails, talk from front to back
+  # backend as api, all of our controllers are in api folder 
+  # this will point from react to somehwhere  in the controller
   
-    # manually
-    # # http verb, url, to: 'controller#action', as: 'nickname'
-    # get '/subs' to: 'subs#index'
+  namespace :api do
+    # this is where all of our routes will be 
+
+    # manually 
+    # http verb, url, to: 'controller#action', as: 'nickname'
+    # get '/subs', to: 'subs#index'
     # get '/sub/:id', to: 'subs#show'
     # post '/subs', to: 'subs#create'
     # put '/sub/:id', to: 'subs#update'
-    # delete '/sub' to: 'subs#destroy'
-    #custom routes - outside of the crud actions
-      #
-    #rails g way
-#     # resources :name of controller
-# get '/url', to: 'controller'
-# get '/ordered-subs', to: 'subs#osubs'
-#handles the main CRUD actions
-resources :subs do
-  resources :topics
-end
+    # delete '/sub/:id', to: 'subs#destroy'
+    # :id - place holder for the actual id value 
+      # custom routes - out side of the crud actions 
+      # get '/url', to: 'controller#action'
+      # get '/ordered-subs', to: 'subs#osubs'
 
+    # rails g way 
+    # resources :name ofcontroller 
+    # handles the main CRUD actions 
+    resources :subs do 
+      resources :topics
+    end
+    #  only do two level deep 
 
+    resources :topics, except: [:index, :show, :create, :update, :destroy] do 
+      resources :comments 
+    end
 
+    # resources :comments, except: [:index, :show, :create, :update, :destroy] do 
+    #   resources :likes 
+    # end
 
-# # model with a parent and child then we need to do a do block
-# # but only do two levels deep
-# resources :parent1 do
-#   resources :child2
-# end
+    # DONT DO
+    # resources :subs do 
+    #   resources :topics do 
+    #     resources :comments do
+    #       resources :likes
+    #     end
+    #   end
+    # end
 
+    # model with a parent and child then we need to 
+    # do a a do block 
+    # but only do two level deep
+    # resources :parent1 do 
+    #   resources :child2
+    # end
 
-# to view the routes in terminal: rails routes
-# recommended: first run your server - bundle exec rails s -p 3001
-# go to the url of http://localhost:3001/rails/info/routes
+    # resources :child2 do 
+    #   resources :child3
+    # end
 
+    # to view the routes 
+  # in terminal - bundle exec rails routes 
+  # recommends - first run your server - 
+    # bundle exec rails s -p 3001
+    # go to the url of http://localhost:3001/rails/info/routes
+    
   end
+
 end
