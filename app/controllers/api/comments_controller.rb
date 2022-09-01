c9f0e257a4c4441ccf0379595265dd52d92c9e46
Christian Green <hoolio.suare@gmail.com>
2022-09-01 15:57:31 -0600

    filled out controller

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -5,21 +5,48 @@ class Api::CommentsController < ApplicationController
 
   #-optional- callback, before_action to clean up the show update and destroy
   def index
-
+    @comment = Comment.all
+    render component: 'Comment',
+    props:{ comment: @comment }
   end
 
   def show
-
+    @comment = Comment.find(params[:id])
+    render component: 'Comment',
+    props: { comment: @comment }
   end
 
   def create
-
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      do something or do somewhere
+      else
+        render component: 'CommentNew',
+        props: { comment: @comment }
+      end
   end
 
   def update
-
+    @comment = Comment.find(params[:id])
+    if @comment.update(comment_params)
+      do something
+      else
+        render component: 'CommentEdit', props: { comment: @comment }
+      end
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    send somewhere
+    or
+    Comment.find(params[:id]).destroy
+    send somewhere
   end
 end
+
+private
+  def comment_params
+
+    params.require(:comment).permit(:attr, :attr2, :everything the table has)
+  end
