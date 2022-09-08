d8f3eac0c3a1a77fd7de3a68cfd5a031d18e4710
Christian Green <hoolio.suare@gmail.com>
2022-09-07 20:00:17 -0600

    delete, add and index/show working

--- client/src/components/subs/Subs.js
+++ client/src/components/subs/Subs.js
@@ -1,24 +1,56 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import SubList from './SubList';
+import SubForm from './SubForm';
 
-
+// this is where all the CRUD are in the front end 
 const Subs = () => {
-  //where we store subs
+  // where we are storing all the subs in the front end
   const [subs, setSubs] = useState([])
 
-  // This will run before we display to the page
+  // this will run before we display to the page
   useEffect( () => {
-    // http verb and url
+    // http verb and url 
     // subs index action
     // res -> all of your subs from your db
     axios.get('/api/subs')
-    .then( res => setSubs(res.data)) //storing all the subs from the db to the component state
-    .catch( err => console.log(err) )
+      .then( res => setSubs(res.data) ) // storing all the subs from the db to the component state
+      .catch( err => console.log(err) )
   }, [])
-  
+
+  // add function with a post request and http
+  const addSub = (sub) => {
+    // create it in the back via the routes and controller
+    axios.post('/api/subs', { sub })
+      .then( res => setSubs([...subs, res.data])) // adding to the front end
+      .catch( err => console.log(err) )
+  }
+
+  const updateSub = (id, sub) => {
+
+  }
+
+  const deleteSub = (id) => {
+    // make sure its back ticks 
+    // interpolting the id we are passing in
+    // destroy in the back end
+    axios.delete(`/api/subs/${id}`)
+      .then( res => {
+        // delete in the front end 
+        // filter out the one delete, or filter in the all the one but the one we deleted
+        setSubs(subs.filter( s => s.id !== id ))
+      })
+      .catch( err => console.log(err) )
+  }
+
   return (
     <>
-      <h1>All Sub reddits</h1>
+      <h1>All Sub Reddits</h1>
+      <SubForm addSub={addSub} />
+      <SubList 
+        subs={subs}
+        deleteSub={deleteSub}
+      />
     </>
   )
 }
